5f231318410654de733bfc4d2977d58611a220ce
RonaldsonBellande <ronaldsonbellande@gmail.com>
2024-03-22 16:45:47 -0400

    latest pushes

--- unzip_linux_mac.sh
+++ unzip_linux_mac.sh
@@ -1,2 +1,7 @@
-unzip ./Bellande_Browser_Application-linux-x64/Bellande_Browser_Application.zip
-unzip ./Bellande_Browser_Application-mac-x64/Bellande_Browser_Application.app/Contents/MacOS/Bellande_Browser_Application.zip
+#!/bin/bash
+
+# Unzip Linux executable
+unzip ./Bellande_Browser_Application-linux-x64/Bellande_Browser_Application.zip -d ./Bellande_Browser_Application-linux-x64/
+
+# Unzip macOS executable
+unzip ./Bellande_Browser_Application-mac-x64/Bellande_Browser_Application.app/Contents/MacOS/Bellande_Browser_Application.zip -d ./Bellande_Browser_Application-mac-x64/Bellande_Browser_Application.app/Contents/MacOS/
